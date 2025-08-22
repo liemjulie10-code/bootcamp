812d147611bff20a8a77370d146ef970bec7f699
Liem Julie <liemjulie10@gmail.com>
2025-08-22 15:08:20 +0300

    יום 2 - רשימות ומילונים

---
+++ day2.py
@@ -0,0 +1,6 @@
+shopping = ["apples", "bread", "milk"]
+shopping.append("eggs")
+print("Shopping list:", shopping)
+person = {"name": "Liem", "age": 21}
+person["city"] = "Tel Aviv"
+print("Person:", person)
